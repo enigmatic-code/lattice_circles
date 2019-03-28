3c19f6fc85943d789ff5b1182bf8600b91e3b4a8
Jim Randell <jim.randell@gmail.com>
2019-03-28 09:55:20 +0000

    minimal setup config.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 # using the following requirements.txt file:
 #
 # --
-# git+https://github.com/enigmatic-code/lattice-circles.git#egg=lattice_circles
+# git+https://github.com/enigmatic-code/lattice_circles.git#egg=lattice_circles
 # --
 
 from distutils.core import setup
 
 # minimal setup config
-setup(name='lattice-circles', py_modules=['lattice_circles'])
+setup(name='lattice_circles', py_modules=['lattice_circles'])
