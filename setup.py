2251b1e27d095ff1b5446ecf129782ec898addcc
Jim Randell <jim.randell@gmail.com>
2019-03-28 09:52:51 +0000

    initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# this file allows lattice_circles.py to be installed directly from GitHub
+#
+# using the following requirements.txt file:
+#
+# --
+# git+https://github.com/enigmatic-code/lattice-circles.git#egg=lattice_circles
+# --
+
+from distutils.core import setup
+
+# minimal setup config
+setup(name='lattice-circles', py_modules=['lattice_circles'])
