c0522d3e76a759976b29aae84df00b7988234e79
Jim Randell <jim.randell@gmail.com>
2020-03-10 10:03:25 +0000

    change to using setuptools (for poetry).

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 # this file allows lattice_circles.py to be installed directly from GitHub
 #
-# using the following requirements.txt file:
+# using Poetry, add the following in pyproject.toml file:
 #
+# --
+# [tool.poetry.dependencies.lattice_circles]
+# git = "https://github.com/enigmatic-code/lattice_circles.git"
+# --
+#
+#
+# [[[ Formerly adding this to requirements.txt worked:
 # --
 # git+https://github.com/enigmatic-code/lattice_circles.git#egg=lattice_circles
 # --
+# but not any more. ]]]
 
-from distutils.core import setup
+from setuptools import setup
 
 # minimal setup config
 setup(name='lattice_circles', py_modules=['lattice_circles'])
